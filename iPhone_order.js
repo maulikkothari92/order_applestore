1226b44e9580ba495c8686d3fe10a224f66a4f33
Maulik Kothari <maulikkothari92@gmail.com>
2014-10-07 08:44:33 -0400

    Stopping on payment error

--- iPhone_order.js
+++ iPhone_order.js
@@ -31,6 +31,28 @@ start_spooky = function (order, callback){
             var color = order["products"]["color"];
             var storage = order["products"]["storage"];
             var max_price = order["max_price"];
+            var shipping_firstname = order["shipping_address"]["first_name"];
+            var shipping_lastname = order["shipping_address"]["last_name"];
+            var shipping_address1 = order["shipping_address"]["address_line1"];
+            var shipping_address2 = order["shipping_address"]["address_line2"];
+            var shipping_postalcode = order["shipping_address"]["zip_code"];
+            var shipping_phone = order["shipping_address"]["phone"];
+            var shipping_area = shipping_phone.substr(0,3);
+            var shipping_phone = shipping_phone.substr(3,9);
+
+            var billing_firstname = order["billing_address"]["first_name"];
+            var billing_lastname = order["billing_address"]["last_name"];
+            var billing_address1 = order["billing_address"]["address_line1"];
+            var billing_address2 = order["billing_address"]["address_line2"];
+            var billing_postalcode = order["billing_address"]["zip_code"];
+            var billing_phone = order["billing_address"]["phone"];
+            var billing_area = billing_phone.substr(0,3);
+            var billing_phone = billing_phone.substr(3,9);
+
+            var credit_card_month = order["payment_method"]["expiration_month"];
+            var credit_card_number = order["payment_method"]["number"];
+            var credit_card_year = order["payment_method"]["expiration_year"];
+            var credit_card_cvv = order["payment_method"]["security_code"];
             
 
             spooky.start(IPHONE_ORDER);
@@ -153,21 +175,21 @@ start_spooky = function (order, callback){
                     this.emit('error', 'max_price_exceeded');
                 }
 
-                this.emit('message', "Trying to click the CONFIRM button");   
+                this.emit('message', "Trying to click the Checkout Now button");   
 
                 this.evaluate(function() {      
                     document.getElementById("checkout-now").click()
-                    console.log('Clicking the CONFIRM button');
+                    console.log('Clicking the Checkout now button');
                 });
                 
             }]);
 
             spooky.then(function(){
-               this.wait(2000);
+               this.wait(1000);
              });
 
             spooky.then(function(){
-                this.capture('captures/[Add to Cart] After clicking checkout button.png');
+                this.capture('captures/[Add to Cart] After clicking checkout-now button.png');
             });
 
 
@@ -176,7 +198,7 @@ start_spooky = function (order, callback){
             });
 
             spooky.then(function(){
-               this.wait(2000);
+               this.wait(1000);
              });
 
             spooky.then(function(){
@@ -188,7 +210,8 @@ start_spooky = function (order, callback){
             });
 
             spooky.then(function(){
-               this.wait(2000);
+               this.waitForSelector("#shipping-user-firstName");
+               this.waitForSelector("#shipping-user-lastName");
              });
 
             spooky.then(function(){
@@ -196,36 +219,37 @@ start_spooky = function (order, callback){
             });
 
 
-            spooky.thenEvaluate(function(){
-                document.getElementById("shipping-user-firstName").value = "Maulik"
-                document.getElementById("shipping-user-lastName").value = "Kothari"
-            });
+            spooky.thenEvaluate(function( shipping_firstname, shipping_lastname){
+                console.log("Entering shipping details");
+                document.getElementById("shipping-user-firstName").value = shipping_firstname;
+                document.getElementById("shipping-user-lastName").value = shipping_lastname;
+            }, { shipping_firstname: shipping_firstname,
+               shipping_lastname: shipping_lastname
+               });
 
             spooky.then(function(){
-               this.wait(2000);
+               this.wait(1000);
              });
 
-            spooky.thenEvaluate(function(){
-                document.getElementById("shipping-user-daytimePhoneAreaCode").value = "812"
-                document.getElementById("shipping-user-daytimePhone").value = "3251316"
-            });
+            spooky.thenEvaluate(function(shipping_area, shipping_phone){
+                document.getElementById("shipping-user-daytimePhoneAreaCode").value = shipping_area;
+                document.getElementById("shipping-user-daytimePhone").value = shipping_phone;
+            }, { shipping_area: shipping_area,
+                 shipping_phone: shipping_phone
+                });
 
-             spooky.thenEvaluate(function(){
-                document.getElementById("shipping-user-postalCode").value = "47408"
-            });
+             spooky.thenEvaluate(function(shipping_postalcode){
+                document.getElementById("shipping-user-postalCode").value = shipping_postalcode;
+            }, {shipping_postalcode: shipping_postalcode
+                });
 
+            spooky.thenEvaluate(function( shipping_address1, shipping_address2){
+                document.getElementById("shipping-user-street").value = shipping_address1;
+                document.getElementById("shipping-user-street2").value = shipping_address2;
+            }, { shipping_address1: shipping_address1,
+               shipping_address2: shipping_address2
+               }); 
 
-             spooky.thenEvaluate(function(){
-                document.getElementById("shipping-user-street").value = "2667 E. 7th Street"
-                document.getElementById("shipping-user-street2").value = "Apt E"
-                //document.getElementById("shipping-user-postalCode").value = "47408"
-            });
- 
-
-
-             spooky.thenEvaluate(function(){
-                document.getElementById("shipping-continue-button").click()
-            });
 
             spooky.then(function(){
                this.wait(1000);
@@ -238,6 +262,10 @@ start_spooky = function (order, callback){
             spooky.then(function(){
                this.wait(2000);
              });
+
+            spooky.thenEvaluate(function(){
+                document.getElementById("shipping-continue-button").click()
+            });
 
             spooky.then(function(){
                 this.capture('captures/[Add to Cart] After filling address.png');
@@ -247,27 +275,49 @@ start_spooky = function (order, callback){
                this.wait(1000);
              });
 
-
-            spooky.thenEvaluate(function(){
-                document.getElementById("payment-credit-user-copy-from-shipping-button").click()
-            });
+            spooky.thenEvaluate(function( billing_firstname, billing_lastname){
+                console.log("Entering billing details");
+                document.getElementById("payment-credit-user-address-firstName").value = billing_firstname;
+                document.getElementById("payment-credit-user-address-lastName").value = billing_lastname;
+            }, { billiing_firstname: billing_firstname,
+               billing_lastname: billing_lastname
+               });    
 
 
             spooky.then(function(){
                this.wait(1000);
              });
 
-            spooky.thenEvaluate(function(){
-                document.getElementById("payment-credit-method-cc0-cardNumber").value = "6011005071946985"
+
+            spooky.thenEvaluate(function(billing_area, billing_phone){
+                console.log("Entering the phone details.");
+                document.getElementById("payment-credit-user-daytimePhoneAreaCode").value = billing_area;
+                document.getElementById("payment-credit-user-daytimePhone").value = billing_phone;
+
+            }, { billing_area: billing_area,
+                 billing_phone: billing_phone
+                });
+
+            spooky.thenEvaluate(function(credit_card_number){
+                console.log("Entering credit_card_number");
+                document.getElementById("payment-credit-method-cc0-cardNumber").value = credit_card_number
+            },{
+                credit_card_number: credit_card_number
             });
 
 
-            spooky.thenEvaluate(function(){
-                document.getElementById("payment-credit-method-cc0-expirationMonth").value = "01"
+            spooky.thenEvaluate(function(credit_card_month){
+                console.log("Entering credit_card_month");
+                document.getElementById("payment-credit-method-cc0-expirationMonth").value = credit_card_month
+            }, {
+                credit_card_month: credit_card_month
             });
             
-            spooky.thenEvaluate(function(){
-                document.getElementById("payment-credit-method-cc0-expirationYear").value = "2017"
+            spooky.thenEvaluate(function(credit_card_year){
+                console.log("Entering credit_card_year")
+                document.getElementById("payment-credit-method-cc0-expirationYear").value = credit_card_year
+            },{
+                credit_card_year: credit_card_year
             });
 
             spooky.then(function(){
@@ -275,23 +325,66 @@ start_spooky = function (order, callback){
              });
 
 
-            spooky.thenEvaluate(function(){
-                document.getElementById("payment-credit-method-cc0-security-code").value = "800"
-            });
+           spooky.thenEvaluate(function(credit_card_cvv){
+                console.log("Entering credit card security code");
+                document.getElementById("payment-credit-method-cc0-security-code").value = credit_card_cvv;
+            }, {
+                credit_card_cvv: credit_card_cvv
+
+                });
 
 
+
             spooky.thenEvaluate(function(){
-                document.getElementById("payment-credit-user-address-emailAddress").value = "mkothari@indiana.edu"
+                console.log("Entering email")
+                document.getElementById("payment-credit-user-address-emailAddress").value = "mkothari9090@indiana.edu"
             });
 
             spooky.then(function(){
                this.wait(1000);
              });
+
+             spooky.thenEvaluate(function(billing_postalcode){
+                console.log("Entering postal details")
+                document.getElementById("payment-credit-user-address-postalCode").value = billing_postalcode;
+            }, {billing_postalcode: billing_postalcode
+                });
+
+            spooky.thenEvaluate(function( billing_address1, billing_address2){
+                console.log("Entering billing details")
+                document.getElementById("payment-credit-user-address-street").value = billing_address1;
+                document.getElementById("payment-credit-user-address-street2").value = billing_address2;
+            }, { billing_address1: billing_address1,
+               billing_address2: billing_address2
+               });
+
+            spooky.then(function(){
+               this.waitForSelector('#payment-credit-user-address-daytimePhoneAreaCode');
+               this.waitForSelector('#payment-credit-user-address-daytimePhone');
+             });
+
+
+            spooky.thenEvaluate(function(billing_area, billing_phone){
+                console.log("Entering the phone details.");
+                console.log(billing_phone);
+                console.log(billing_area);
+                document.getElementById("payment-credit-user-address-daytimePhoneAreaCode").value = billing_area;
+                document.getElementById("payment-credit-user-address-daytimePhone").value = billing_phone;
+
+            }, { billing_area: billing_area,
+                 billing_phone: billing_phone
+                });    
+
 
             spooky.then(function(){
                 this.capture('captures/[Add to Cart] Before payment.png');
             });
 
+            spooky.then(function(){
+               this.waitForSelector('#payment-continue-button');
+             });
+
+
             spooky.thenEvaluate(function(){
                 document.getElementById("payment-continue-button").click()
             });
@@ -300,21 +393,16 @@ start_spooky = function (order, callback){
                this.wait(1000);
              });
 
-            spooky.thenEvaluate(function(){
-                document.getElementById("payment-credit-method-cc0-security-code").value = "800"
+            spooky.thenEvaluate(function(credit_card_cvv){
+                document.getElementById("payment-credit-method-cc0-security-code").value = credit_card_cvv;
+            }, {
+                credit_card_cvv: credit_card_cvv
             });
 
             spooky.then(function(){
-               this.wait(1000);
+               this.waitForSelector('#payment-continue-button');
              });
 
-            spooky.thenEvaluate(function(){
-                document.getElementById("payment-continue-button").click()
-            });
-
-            spooky.then(function(){
-               this.wait(1000);
-             });
 
             spooky.thenEvaluate(function(){
                 document.getElementById("payment-continue-button").click()
@@ -325,8 +413,86 @@ start_spooky = function (order, callback){
              });
 
             spooky.then(function(){
-                this.capture('captures/[Add to Cart] Before payment.png');
+                this.capture('captures/[Add to Cart] After payment.png');
             });
+
+            spooky.then([{
+                  max_price: max_price
+                },function(){
+                var price_on_page = this.evaluate(function() {      
+                  return document.getElementById("cart-summary-order-total-value").innerHTML
+                });
+                price_on_page = price_on_page.replace(/\$/g, '')
+                price_on_page = parseInt(price_on_page);
+                max_price = parseInt(max_price);
+
+                if(price_on_page > max_price)
+                {
+                    this.emit('error', 'max_price_exceeded');
+                }
+
+                this.emit('message', "Trying to click the Checkout Now button");   
+
+                this.evaluate(function() {      
+                    document.getElementById("account-continue-as-guest").click()
+                    console.log('Clicking the Continue as Guest button');
+                });
+                
+            }]);
+
+            spooky.then(function(){
+               this.wait(2000);
+             });
+
+
+            spooky.then(function(){
+                this.capture('captures/[Add to Cart] After the last continue.png');
+            });
+
+            spooky.then(function(){
+               this.waitForSelector('#place-order-button');
+             });
+
+
+            spooky.then(function(){
+               this.wait(2000);
+             });
+
+
+            spooky.thenEvaluate(function(){
+                console.log("Clicking the PLACE ORDER BUTTON");
+                document.getElementById("place-order-button").click()
+            });
+
+            spooky.then(function(){
+               this.wait(10000);
+             });
+
+            spooky.then(function(){
+                this.capture('captures/[Add to Cart] After clicking place order.png');
+            });
+
+
+            spooky.then(function(){
+                var invalid_details = this.evaluate(function(){
+                    return document.getElementById("payment-error").innerHTML;
+                });
+
+                if(typeof invalid_details != 'undefined'){
+
+                    this.emit('error', 'invalid_details');
+                } 
+
+
+            });
+
+
+            spooky.then(function(){
+                this.capture('captures/[Final Page] Payment Error caught????.png');
+            });
+
+    
+            
 
 
 
